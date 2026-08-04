c177126c827be8fe05406dd5bbdbf89d67db13ec
leeichiro28 <leeichilu28@gmail.com>
2026-08-04 09:32:56 +0800

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,4 +0,0 @@
-// 把下面兩個值換成你自己 Supabase 專案的
-// Settings → API 裡的 Project URL 和 anon public key
-window.SUPABASE_URL = "https://samhdvaqmkhywalgnxon.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_MhznDRra8nOIRfwxxqsV4g_KZgruZB8";
